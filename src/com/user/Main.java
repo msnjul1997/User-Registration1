a669a5627e252a18ee799b972fd59752face412b
msnkittu859 <Vamshikalyan97>
2022-06-16 16:46:46 +0530

    User-Registration

---
+++ src/com/user/Main.java
@@ -0,0 +1,12 @@
+package com.user;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("User-Regestrtion");
+        Validation v = new Validation();
+        Scanner sc = new Scanner(System.in);
+	}
+
+}
---
+++ src/com/user/Validation.java
@@ -0,0 +1,16 @@
+package com.user;
+
+public class Validation {
+	 public void check(boolean ans)
+	    {
+	        if(ans)
+	        {
+	            System.out.println("Valid");
+	        }
+	        else
+	        {
+	            System.out.println("Not valid");
+	        }
+	    }
+
+}
