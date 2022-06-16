1c24f0d23ad1d14b6bb316773d3c218db151fce4
msnkittu859 <Vamshikalyan97>
2022-06-16 17:22:28 +0530

    Added: Min 8 Char & One UpperCase

--- src/com/user/Main.java
+++ src/com/user/Main.java
@@ -7,6 +7,33 @@ public class Main {
 		System.out.println("User-Regestrtion");
         Validation v = new Validation();
         Scanner sc = new Scanner(System.in);
-	}
+        
+        
+        System.out.println("Enter first name: ");
+        String fname = sc.next();
+        v.nameValidation(fname);
+        
 
+        System.out.println("Enter last name: ");
+        String lname = sc.next();
+        v.nameValidation(lname);
+        
+        System.out.println("Enter your Email-ID: ");
+        String email = sc.next();
+        v.emailValidation(email);
+        
+        System.out.println("Enter your 10 digit phone number: ");
+        String phone = sc.next();
+        v.phoneNumberValidation(phone);
+        
+        System.out.println("Enter your password(it should be of minimum length 8): ");
+        String password1 = sc.next();
+        v.passwordValidation(password1);
+        
+        System.out.println("Enter your password(it should be of min length 8 & One UpperCase): ");
+        String password = sc.next();
+        v.passwordValidation(password);
+
+	}
+	
 }
--- src/com/user/Validation.java
+++ src/com/user/Validation.java
@@ -1,5 +1,8 @@
 package com.user;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Validation {
 	 public void check(boolean ans)
 	    {
@@ -12,5 +15,42 @@ public class Validation {
 	            System.out.println("Not valid");
 	        }
 	    }
-
+	 public void nameValidation(String name)
+	    {
+	        String regex ="^[A-Z]{1}+[a-z]{2,}$";
+	        Pattern pt = Pattern.compile(regex);
+	        Matcher mt = pt.matcher(name);
+	        check(mt.find());
+	    }
+	 public void emailValidation(String email)
+	    {
+	        String regex = "^[abc]{1}[A-Za-z0-9]*@{1}[A-Za-z0-9]{1,}.com{1}";
+	        Pattern pt = Pattern.compile(regex);
+	        Matcher mt = pt.matcher(email);
+	        check(mt.find());
+	    }
+	 
+	 public void phoneNumberValidation(String phone)
+	    {
+	        String regex = "^(91|0)?+[7-9]{1}+[0-9]{9}$";
+	        Pattern pt = Pattern.compile(regex);
+	        Matcher mt = pt.matcher(phone);
+	        check(mt.find());
+	    }
+	 public void passwordValidation1(String password)
+	    {
+	        // Rule 1 = Minimum 8 characters 
+	        String regex = "^(?=.[a-z0-9!@#&()–[{}]:;',?/*~$^+=<>])(?=.*[A-Z]).{8,}";
+	        Pattern p = Pattern.compile(regex);
+	        Matcher m = p.matcher(password);
+	        check(m.find());
+	    }
+	 public void passwordValidation(String password)
+	    {
+		    // Rule 2 = Min 8 Characters & One UpperCase
+		    String regex = "^(?=.[a-z0-9!@#&()–[{}]:;',?/*~$^+=<>])(?=.*[A-Z]).{8,}";
+	        Pattern p = Pattern.compile(regex);
+	        Matcher m = p.matcher(password);
+	        check(m.find());
+	    }
 }
